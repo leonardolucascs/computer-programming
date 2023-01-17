f95d861a49644de2527469c3a47af85cfed10253
leonardolucascs <leonardolucas.cs@icloud.com>
2023-01-17 23:17:50 +0100

    Upload files

---
+++ lab09/mappe/output2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main(){
+	mappa := make(map[string]int)
+	// equivalente a: mappa := map[string]int{}
+
+	mappa["A"] = 10
+	mappa["B"] -= 5
+	mappa["F"] = 0
+	mappa["D"] = mappa["E"] + 5
+
+	if mappa["F"] != 0 {
+		fmt.Printf("F è presente con valore %d\n", mappa["F"])
+	} else {
+		fmt.Printf("F non è presente\n")
+	}
+
+	if v, ok := mappa["C"]; ok {
+		fmt.Printf("C è presente con valore %d\n", v)
+	} else {
+		fmt.Printf("C non è presente\n")
+	}
+
+	if v, ok := mappa["B"]; ok {
+		fmt.Printf("B è presente con valore %d\n", v)
+	} else {
+		fmt.Print("B non è presente\n")
+	}
+
+	delete(mappa, "B")
+	
+	mappa["A"] += 100
+	fmt.Println("Elementi in mappa:")
+	for k := range mappa {
+		fmt.Printf("Chiavi: %s - Valore: %d\n", k, mappa[k])
+	}
+}
