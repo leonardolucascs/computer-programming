fa94dcd861260d1be9b7c5677724cba424639c73
leonardolucascs <leonardolucas.cs@icloud.com>
2023-01-26 22:28:39 +0100

    Upload file

--- aa22.23/lab/prof.Bianchessi/examLab/25.01.23/esercizio_2.go
+++ aa22.23/lab/prof.Bianchessi/examLab/25.01.23/esercizio_2.go
@@ -6,7 +6,7 @@ import "strconv"
 
 func main() {
 
-	var numeri []int
+	var numeri []uint
 
 
 	input := os.Args[1:]
@@ -16,9 +16,9 @@ func main() {
 		numeri = append(numeri, uint(tmp))
 	}
 
-	fmt.Println(numeri)
+	//fmt.Println(numeri)
 
-	fmt.Println(mcm(13, 78))
+	//fmt.Println(mcm(13, 78))
 	fmt.Println(mcm_sequenza(numeri))
 
 }
@@ -37,17 +37,49 @@ func mcm(a, b uint) uint {
 	return (a * b) / MCD(a, b)
 }
 
+
 func mcm_sequenza(numeri []uint) int {
 
+	// non serve alcun tipo di ciclo !!!
+	// Le "iterazioni cicliche" vengono "svolte" dalle chiamate ricorsive
+
+	/*
 	var mcm int
 
 	for i:=0; i<len(numeri)-1; i++{
 		// caso base mcm(a, b)
 		if len(numeri) == 2 {
-			return int(mcm(numeri[i], numeri[i+1]))
+			return int(mcm(numeri[i], numeri[i+1]))	// ERROR: Cannot call non-function mcm => value type int
+													// shadowing variable name & function name!!!
 		} else {
 			mcm = mcm_sequenza(numeri[i:])
 		}
 	}
+	*/
 
+
+	fmt.Println("\nstato iniziale: ", numeri)
+
+		// caso base mcm(n1, n2)
+		if len(numeri)==2{
+			fmt.Printf("...attivazione caso base: mcm(%d, %d)\n\n", numeri[0], numeri[1])
+			
+			output := mcm(numeri[0], numeri[1])
+
+			fmt.Println("mcm(", numeri[0], ", ", numeri[1] ,") => ", output)
+
+			return int(output)	// int( mcm(numeri[0], numeri[1]) )
+		}else{	// passo induttivo:
+			fmt.Println("...chiamata funzione ricorsiva: mcm(", numeri[:len(numeri)-1], ")")
+
+			tmp := uint(mcm_sequenza( numeri[:len(numeri)-1] ) )
+			op := mcm( numeri[len(numeri)-1], tmp )
+			
+			fmt.Println("mcm(", numeri[len(numeri)-1], ", mcm(", numeri[:len(numeri)-1] ,") )")
+			fmt.Println("mcm(", numeri[len(numeri)-1],", ", tmp ,") =>", op)
+			
+			return int(op)	// int( mcm( numeri[len(numeri)-1], uint(mcm_sequenza( numeri[:len(numeri)-1] ) ) ) )
+		}
+	
+	return 0	// necessario se non entra nel for per len(numeri)==0
 }
