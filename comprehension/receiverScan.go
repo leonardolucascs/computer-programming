139ead4884dc18c929cb2e2fde35fe7d23754fa0
leonardolucascs <leonardolucas.cs@icloud.com>
2023-02-01 08:51:44 +0100

    Update file

--- comprehension/receiverScan.go
+++ comprehension/receiverScan.go
@@ -5,16 +5,18 @@ import (
 )
 
 /* 
-	Advanced Topic: RECIEVERS
+	
+	Topic: RECIEVERS
+	
+	GO: Things in parenthesis before function name
+	https://www.linkedin.com/pulse/go-things-parenthesis-before-function-name-shivam-chaurasia/
+
 */
 
 type User struct {
 	Name string
 	Pet  []string
 }
-
-// GO: Things in parenthesis before function name
-// https://www.linkedin.com/pulse/go-things-parenthesis-before-function-name-shivam-chaurasia/
 
 // newPet is a method of type *User, can be invoked by any object of type *User, in this case p2
 // p2.newPet(), it's called receiver
@@ -34,27 +36,29 @@ func main() {
 
 
 /*
-	- WHY & IS NEED FOR SCAN TO MODIFY VALUE OF A VARIABLE
+	Topic: WHY & IS NEED FOR SCAN TO MODIFY VALUE OF A VARIABLE
 	
-
 	It needs to change the variable. Since all arguments in C are passed by value you need to
 	pass a pointer if you want a function to be able to change a parameter.
 
-	Here's a super-simple example showing it:
-*/
+	Here's a super-simple example showing it in C++:
 
-void nochange(int var) {
-    // Here, var is a copy of the original number. &var != &value
-    var = 1337;
-}
-void change(int *var) {
-    // Here, var is a pointer to the original number. var == &value
-    // Writing to `*var` modifies the variable the pointer points to
-    *var = 1337;
-}
-int main() {
-    int value = 42;
-    nochange(value);
-    change(&value);
-    return 0;
-}
+
+	void nochange(int var) {
+	    // Here, var is a copy of the original number. &var != &value
+	    var = 1337;
+	}
+	void change(int *var) {
+	    // Here, var is a pointer to the original number. var == &value
+	    // Writing to `*var` modifies the variable the pointer points to
+	    *var = 1337;
+	}
+	int main() {
+	    int value = 42;
+	    nochange(value);
+	    change(&value);
+	    return 0;
+	}
+
+
+*/
