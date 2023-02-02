7a9cb8b540492b3c8643f66a1984dacd46e5070f
leonardolucascs <leonardolucas.cs@icloud.com>
2023-02-02 12:16:28 +0100

    Update file

--- comprehension/inputStdin.go
+++ comprehension/inputStdin.go
@@ -10,30 +10,44 @@ func main() {
 
 	var a string
 
-	fmt.Println("value a: ", a)
+	fmt.Println("Initial value a: ", a)
 
 	fmt.Print("Enter value a: ")
 	// Lettura singola stringa
 	//nInput, err := fmt.Scan(&a)
-	nInput, err := fmt.Scan(a)
+	nInput, err := fmt.Scan(&a)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("value a: ", a)
-	fmt.Println("n input: ", nInput)
+
+	fmt.Println("\nFinal value a:", a)
+	fmt.Println("n input:", nInput)
 	fmt.Println("err: ", err)
 
-	/*
-		var slA []string
+	// DIGRESSION ON PRINT
 
-		fmt.Print("Enter a string: ")
+	// Println relies on doPrint(args, true, true), where first argument is
+	// addspace and second is addnewline. So Prinln ith multiple arguments will 
+	// always print space.
+	fmt.Println("a","b")
 
-		for i:=0; i<len(slA); i++ {
-			//fmt.Scan(&slA[i])
-			fmt.Scan(slA[i])
-		}
+	// No space added in this case
+	fmt.Print("a", "b\n")
 
-		fmt.Println(slA)
-	*/
+	// With Printf output results alike as right as you expected
+	fmt.Printf("%s %s\n", "a", "b")
+
+
+	var slA []string
+
+	fmt.Println("Enter multiple words[5]\nScan scans text read from standard input, storing successive space-separated values\ninto successive arguments.Newlines count as space.\n")
+	fmt.Print("Enter a string: ")
+
+	for i:=0; i<len(slA); i++ {
+		//fmt.Scan(&slA[i])
+		fmt.Scan(&slA[i])
+	}
+
+	fmt.Println(slA)
 
 }
