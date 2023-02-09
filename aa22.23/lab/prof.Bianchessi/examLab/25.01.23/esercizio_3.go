0d86e032974975b1b210e3911bb0d253b8286c53
leonardolucascs <leonardolucas.cs@icloud.com>
2023-02-09 23:59:28 +0100

    Update file

--- aa22.23/lab/prof.Bianchessi/examLab/25.01.23/esercizio_3.go
+++ aa22.23/lab/prof.Bianchessi/examLab/25.01.23/esercizio_3.go
@@ -1,5 +1,12 @@
 /*
-	Testo: 
+	Parte1:
+	Scrivere un programma che:
+		- legga da standard input una sequenza di righe di testo
+		- termini la lettura quando, premendo la combinazione di tasti Ctrl+D, viene inserito da standard input 
+			l'indicatore End-Of-File(EOF)
+
+		Ogni riga di testo avrà il formato:
+			inizio fine nome
 */
 
 package main
