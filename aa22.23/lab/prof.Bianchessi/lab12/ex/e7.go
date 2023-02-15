3bfe89dda5274ae5b6b0643736d1e0679fa0f650
leonardolucascs <leonardolucas.cs@icloud.com>
2023-02-15 10:06:26 +0100

    Upload file

---
+++ aa22.23/lab/prof.Bianchessi/lab12/ex/e7.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main(){
+
+	scanner := bufio.NewScanner(os.Stdin)
+	LeggiEStampa(scanner)
+}
+
+// Lettura multiple riga da std.input
+func LeggiEStampa(scanner *bufio.Scanner) {
+	// Fin tanto che c'è qualcosa da leggere, stampa
+	if scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println(text)
+		
+		// La chiamata ricorsiva permette la continua lettura da std.input
+		LeggiEStampa(scanner)
+
+		// Premendo la combinazioni tasti CTRL+D, viene inserito da std.input 
+		// l'indicatore di End-Of-File(EOF) definendo così la fine della 
+		// lettura
+	}
+}
+
+// La redirezione dell'output avviene tramite " > " sul terminale
