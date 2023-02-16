ac795be333148c8e5df8311a619979cd65817a5d
leonardolucascs <leonardolucas.cs@icloud.com>
2023-02-16 03:25:22 +0100

    Update files

--- aa22.23/lab/prof.Bianchessi/examLab/16.02.23/consegnato/esercizio_1.go
+++ aa22.23/lab/prof.Bianchessi/examLab/16.02.23/consegnato/esercizio_1.go
@@ -25,8 +25,54 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	inStr, _ := reader.ReadString('\n')
 
-	fmt.Println(inStr, len(inStr))
+	// Nella lettura ReadString() salva il delimitatore dentro la variabile
+	// fmt.Println(inStr, len(inStr))
+	// Per questo occorre effettuare un reslicing
 	inStr = inStr[:len(inStr)-1]
-	fmt.Println(inStr, len(inStr))
+	
+	//fmt.Println(inStr, len(inStr))
 
+	var tmp string
+
+	for _, v := range inStr {
+		if unicode.IsSpace(v) {
+			fmt.Println("Presenza di spazi nella riga letta...")
+			return
+		} else {
+			tmp += string(unicode.ToLower(v))
+		}
+	}
+
+	//fmt.Println(tmp)
+	fmt.Println("Sottostringhe:")
+
+	var sottoStr string
+
+	maxStr := ""
+
+	// Attenzione: questo tipo di controllo tramite for indici 
+	// 				vale però solo per caratteri ASCII
+	
+	// (*) Svolgere tentativo implementazione anche tramite for range
+	// (*)	non riuscito durante esame
+
+	for i:=0; i<len(tmp); i++{
+		for j:=i+1; j<len(tmp); j++{
+			// Laddove trovi una sottostringa, rappresentata da caratteri uguali
+			if tmp[i] == tmp[j] { 
+				sottoStr = tmp[i:j+1]	// l'ultimo carattere non deve essere escluso dallo slicing
+										
+										// Attenzione: stringa tmp convertita in ToLower, l'ouput
+										// 				expected deve essere l'originale
+				if len(sottoStr)>=2 && palindroma(sottoStr) {
+					fmt.Println(i, j, sottoStr)
+					if len(sottoStr) > len(maxStr) {
+						maxStr = sottoStr
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("\nSottostringa di lunghezza massima:\n", maxStr, len(maxStr))
 }
